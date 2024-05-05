45ca8c863fb5f8dcc946d890f8f54c845fd4c563
Manuel Fino <88818021+Akindo-Raccoon@users.noreply.github.com>
2024-05-04 22:07:09 -0500

    Update weather_report.js

--- weather_report.js
+++ weather_report.js
@@ -3,16 +3,16 @@ function showweatherDetails(event) {
 }
 
  const city = document.getElementById('city').value;
-      const apiKey = '0b83a51750da495c2c4bb41581caf3d1'; // Replace 'YOUR_API_KEY' with your actual API key
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      const apiKey = '82f66ee41b1269e1e0a0769fc20687c4'; // Replace 'YOUR_API_KEY' with your actual API key
+      const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric';
 
  fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
           const weatherInfo = document.getElementById('weatherInfo');
-          weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+          weatherInfo.innerHTML = '<h2>Weather in ${data.name}</h2>
                                   <p>Temperature: ${data.main.temp} &#8451;</p>
-                                  <p>Weather: ${data.weather[0].description}</p>`;
+                                  <p>Weather: ${data.weather[0].description}</p>';
         })
 
  document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
